f95db6bb78330afb71690f1284f76cc9275eda16
Raphaël Tétreault <150467892+MohawkRaphaelT@users.noreply.github.com>
2023-11-27 16:22:38 -0500

    Add game UI
    Add text mesh pro library
    Add scripts to draw score to screen
    Add UI structure in scene

--- CoolGame/Assets/Scripts/CoinCounter.cs
+++ CoolGame/Assets/Scripts/CoinCounter.cs
@@ -1,14 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CoinCounter : MonoBehaviour
 {
+    public TMP_Text scoreDisplay;
     private int score;
+
+    public void Start()
+    {
+        DisplayScore();
+    }
 
     public void AddToScore(int points)
     {
         score += points;
-        Debug.Log(score);
+        DisplayScore();
+    }
+
+    public void DisplayScore()
+    {
+        scoreDisplay.text = $"Score: {score:00000000}";
     }
 }
